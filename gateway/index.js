00af9dfb51d66d55015f97c7cbd44ad1e88a139e
Bruno Ramalho <bramalho@divante.pl>
2021-05-06 18:19:46 +0200

    gateway

---
+++ gateway/index.js
@@ -0,0 +1,47 @@
+const {ApolloGateway, RemoteGraphQLDataSource} = require("@apollo/gateway");
+const {ApolloServer} = require("apollo-server-express");
+const express = require("express");
+const expressJwt = require("express-jwt");
+
+const port = 4000;
+const app = express();
+
+app.use(
+    expressJwt({
+        secret: "THIS_IS_MY_SECRET",
+        algorithms: ["HS256"],
+        credentialsRequired: false
+    })
+);
+
+const gateway = new ApolloGateway({
+    serviceList: [
+        {name: "users", url: "http://localhost:4001"}
+    ],
+    buildService({name, url}) {
+        return new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({request, context}) {
+                request.http.headers.set(
+                    "user",
+                    context.user ? JSON.stringify(context.user) : null
+                );
+            }
+        });
+    }
+});
+
+const server = new ApolloServer({
+    gateway,
+    subscriptions: false,
+    context: ({req}) => {
+        const user = req.user || null;
+        return {user};
+    }
+});
+
+server.applyMiddleware({app});
+
+app.listen({port}, () =>
+    console.log(`🚀 Server running at http://localhost:${port}${server.graphqlPath}`)
+);
